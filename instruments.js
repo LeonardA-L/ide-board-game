5bbdd1c5f134fd61fb12db7645c529e0ea462307
LeonardA-L <leonard@allain-launay.fr>
2017-09-27 20:03:17 +0200

    Put instruments in a sperate file

---
+++ instruments.js
@@ -0,0 +1,27 @@
+var instruments = {
+	"melody": [{
+		src: "sounds/melody_1.mp3",
+		name: "The Flute"
+	},
+	{
+		src: "sounds/melody_2.mp3",
+		name: "The Violin"
+	}],
+	"bass":   [{
+		src: "sounds/bass_1.mp3",
+		name: "The Bass"
+	},
+	{
+		src: "sounds/bass_2.mp3",
+		name: "The Trumpet"
+	}],
+	"drums":  [{
+		src: "sounds/drum_1.mp3",
+		name: "Kicks"
+	},
+	{
+		src: "sounds/drum_2.mp3",
+		name: "Claps"
+	}],
+	"effects": []
+};
